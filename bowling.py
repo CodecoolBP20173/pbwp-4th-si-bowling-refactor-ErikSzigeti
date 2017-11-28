a6171461f83d225d8c07d7e592ca8f353c356e91
Erik Szigeti <erik.szigeti01@gmail.com>
2017-11-28 13:36:38 +0100

    result_getter function

--- bowling.py
+++ bowling.py
@@ -2,20 +2,9 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
+    last = 0
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
-        else:
-            result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
+        result = result_getter(game, i, last, frame, result)
         last = get_value(game[i])
         if in_first_half:
             in_first_half = False
@@ -25,6 +14,23 @@ def score(game):
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
+    return result
+
+
+def result_getter(game, i, last, frame, result):
+    if game[i] == '/':
+        result += 10 - last
+    else:
+        result += get_value(game[i])
+    if frame < 10 and get_value(game[i]) == 10:
+        if game[i] == '/':
+            result += get_value(game[i+1])
+        elif game[i] == 'X' or game[i] == 'x':
+            result += get_value(game[i+1])
+            if game[i+2] == '/':
+                result += 10 - get_value(game[i+1])
+            else:
+                result += get_value(game[i+2])
     return result
 
 
